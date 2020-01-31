3c3f48c7a0cc71c83bed662aa207b36fc2090071
Naveed Ashfaq <naveedashfaq914@gmail.com>
2020-01-30 19:56:04 -0500

    First implementations of solution.py

--- Lab 1/solution.py
+++ Lab 1/solution.py
@@ -30,7 +30,18 @@ def heur_manhattan_distance(state):
     #When calculating distances, assume there are no obstacles on the grid.
     #You should implement this heuristic function exactly, even if it is tempting to improve it.
     #Your function should return a numeric value; this is the estimate of the distance to the goal.
-    return 0
+    manhattan_distance = 0
+    for box in state.boxes:
+      min_dist = 10000  # Just a large number, interprete as infinity
+      for storage in state.storage:
+        dist = abs(box[0] - storage[0]) + abs(box[1] - storage[1])
+        if dist < min_dist:
+          min_dist = dist
+      manhattan_distance += min_dist
+    
+    return manhattan_distance
+
+
 
 
 #SOKOBAN HEURISTICS
@@ -52,7 +63,145 @@ def heur_alternate(state):
     #heur_manhattan_distance has flaws.
     #Write a heuristic function that improves upon heur_manhattan_distance to estimate distance between the current state and the goal.
     #Your function should return a numeric value for the estimate of the distance to the goal.
-    return 0
+    h = 0
+    robot_positions = []
+    remaining_storages = []
+    robot_positions.extend(state.robots)
+    remaining_storages.extend(state.storage)
+    for box in state.boxes:
+      if box in state.storage:
+        continue
+      #if not box_is_movable(state, box):
+      if unmoveable(state, box):
+        return float('inf')
+      closest_robot = float('inf')
+      closest_robot_dist = float('inf')
+      closest_storage = float('inf')
+      closest_storage_dist = float('inf')
+      for i in range(len(robot_positions)):
+        robot = robot_positions[i]
+        dist = abs(box[0] - robot[0]) + abs(box[1] - robot[1])
+        if dist < closest_robot_dist:
+          closest_robot_dist = dist
+          closest_robot = i
+      for j in range(len(remaining_storages)):
+        storage = remaining_storages[j]
+        #if storage in state.boxes:
+        #  continue
+        dist = abs(box[0] - storage[0]) + abs(box[1] - storage[1])
+        if dist < closest_storage_dist:
+          closest_storage_dist = dist
+          closest_storage = j
+      #if len(remaining_storages) > 0:
+      robot_positions[closest_robot] = remaining_storages[closest_storage]
+      remaining_storages.pop(closest_storage)
+      h += closest_robot_dist + closest_storage_dist
+    return h
+
+#Test:
+def unmoveable(state, box):
+    ''' Check whether the box is in some place that unable to move.'''
+
+    up_wall = False
+    down_wall = False
+    left_wall = False
+    right_wall = False
+    diag_wall = False
+    up_box = False
+    down_box = False
+    left_box = False
+    right_box = False
+    diag_box = False
+
+    up = (box[0], box[1] - 1)
+    down = (box[0], box[1] + 1)
+    left = (box[0] - 1, box[1])
+    right = (box[0] + 1, box[1])
+    diag = (box[0] + 1, box[1] + 1)
+
+    if box[0] == 0 or left in state.obstacles:
+        left_wall = True
+    elif left in state.boxes:
+        left_box = True
+    if box[0] == state.width - 1 or right in state.obstacles:
+        right_wall = True
+    elif right in state.boxes:
+        right_box = True
+    if box[1] == 0 or down in state.obstacles:
+        down_wall = True
+    elif down in state.boxes:
+        down_box = True
+    if box[1] == state.height - 1 or up in state.obstacles:
+        up_wall = True
+    elif up in state.boxes:
+        up_box = True
+    if diag in state.boxes:
+        diag_box = True
+    elif (right_wall or down_wall) or diag in state.obstacles:
+        diag_wall = True
+
+    if ((up_wall or down_wall) and (right_wall or left_wall or right_box or left_box))or((right_wall or left_wall) \
+       and (up_box or down_box)) or ((down_wall or down_box) and (diag_wall or diag_box) and (right_wall or right_box)):
+        return True
+    return False
+
+def box_is_movable(state, box):
+  return can_move_up(state, box) or can_move_down(state, box) or can_move_left(state, box) or can_move_right(state, box)
+
+def can_move_up(state, box):
+  if (box[1] == 0):
+    return False
+  new_location = (box[0], box[1]-1)
+  if (new_location in state.obstacles):
+    return False
+  if (new_location in state.boxes):
+    #return can_move_left(state, new_location) or can_move_right(state, new_location) or can_move_up(state, new_location)
+    return False
+  return True
+
+def can_move_down(state, box):
+  if (box[1] == state.height-1):
+    return False
+  new_location = (box[0], box[1]+1)
+  if (new_location in state.obstacles):
+    return False
+  if (new_location in state.boxes):
+    #return can_move_left(state, new_location) or can_move_right(state, new_location) or can_move_down(state, new_location)
+    return False
+  return True
+
+def can_move_left(state, box):
+  if (box[0] == 0):
+    return False
+  new_location = (box[0]-1, box[1])
+  if (new_location in state.obstacles):
+    return False
+  if (new_location in state.boxes):
+    #return can_move_left(state, new_location) or can_move_up(state, new_location) or can_move_down(state, new_location)
+    return False
+  return True
+
+def can_move_right(state, box):
+  if (box[0] == state.width-1):
+    return False
+  new_location = (box[0]+1, box[1])
+  if (new_location in state.obstacles):
+    return False
+  if (new_location in state.boxes):
+    #return can_move_up(state, new_location) or can_move_right(state, new_location) or can_move_down(state, new_location)
+    return False
+  return True
+
+def box_in_corner(state, box):
+  if box[0] == 0 and box[1] == 0:
+    return True
+  if box[0] == 0 and box[1] == state.height-1:
+    return True
+  if box[0] == state.width-1 and box[1] == 0:
+    return True
+  if box[0] == state.width-1 and box[1] == state.height-1:
+    return True
+  return False
 
 def heur_zero(state):
     '''Zero Heuristic can be used to make A* search perform uniform cost search'''
@@ -75,15 +224,34 @@ def fval_function(sN, weight):
     #The function must return a numeric f-value.
     #The value will determine your state's position on the Frontier list during a 'custom' search.
     #You must initialize your search engine object as a 'custom' search engine if you supply a custom fval function.
-    return 0
+    return sN.gval + weight * sN.hval
 
-def anytime_weighted_astar(initial_state, heur_fn, weight=1., timebound = 10):
+def anytime_weighted_astar(initial_state, heur_fn, weight=5., timebound = 10):
 #IMPLEMENT
   '''Provides an implementation of anytime weighted a-star, as described in the HW1 handout'''
   '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
   '''OUTPUT: A goal state (if a goal is found), else False'''
   '''implementation of weighted astar algorithm'''
-  return False
+  start = os.times()[0]
+  se = SearchEngine('astar', 'path')
+  wrapped_fval_function = (lambda sN : fval_function(sN, weight))
+  se.init_search(initial_state, sokoban_goal_state, heur_fn, wrapped_fval_function)
+  best_solution = se.search(timebound)
+  if not best_solution:
+    return False
+  new_timebound = timebound - (os.times()[0] - start)
+  while new_timebound > 0:
+      weight /= 2
+      if weight < 1:
+        weight = 1
+      new_solution = se.search(new_timebound, (best_solution.gval, float('inf'), best_solution.gval))
+      if not new_solution:
+          return best_solution
+      best_solution = new_solution
+      if weight == 1:
+        return best_solution
+      new_timebound = timebound - (os.times()[0] - start)
+  return best_solution
 
 def anytime_gbfs(initial_state, heur_fn, timebound = 10):
 #IMPLEMENT
@@ -91,4 +259,17 @@ def anytime_gbfs(initial_state, heur_fn, timebound = 10):
   '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
   '''OUTPUT: A goal state (if a goal is found), else False'''
   '''implementation of weighted astar algorithm'''
-  return False
+  start = os.times()[0]
+  se = SearchEngine('best_first', 'path')
+  se.init_search(initial_state, sokoban_goal_state, heur_fn)
+  best_solution = se.search(timebound)
+  if not best_solution:
+      return False
+  new_timebound = timebound - (os.times()[0] - start)
+  while new_timebound > 0:
+      new_solution = se.search(new_timebound, (best_solution.gval, best_solution.gval, best_solution.gval))
+      if not new_solution:
+          return best_solution
+      best_solution = new_solution
+      new_timebound = timebound - (os.times()[0] - start)
+  return best_solution
