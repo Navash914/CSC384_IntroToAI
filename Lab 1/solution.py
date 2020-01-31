83df4cc496937621f6c055defae73737e5e1a591
Naveed Ashfaq <naveedashfaq914@gmail.com>
2020-01-31 01:11:44 -0500

    Completed heuristic function. Lab 1 completed

--- Lab 1/solution.py
+++ Lab 1/solution.py
@@ -32,7 +32,7 @@ def heur_manhattan_distance(state):
     #Your function should return a numeric value; this is the estimate of the distance to the goal.
     manhattan_distance = 0
     for box in state.boxes:
-      min_dist = 10000  # Just a large number, interprete as infinity
+      min_dist = float('inf')
       for storage in state.storage:
         dist = abs(box[0] - storage[0]) + abs(box[1] - storage[1])
         if dist < min_dist:
@@ -63,99 +63,118 @@ def heur_alternate(state):
     #heur_manhattan_distance has flaws.
     #Write a heuristic function that improves upon heur_manhattan_distance to estimate distance between the current state and the goal.
     #Your function should return a numeric value for the estimate of the distance to the goal.
-    h = 0
-    robot_positions = []
-    robot_positions.extend(state.robots)
-    remaining_storages = [st for st in state.storage if st not in state.boxes]
+    h = 0   # heuristic value
+    robot_positions = [robot for robot in state.robots]   # Current position of robots
+    remaining_storages = [st for st in state.storage if st not in state.boxes]   # Remaining storages for boxes to go into
+
     for box in state.boxes:
       if box in state.storage:
-        continue
+        continue    # Box is already in storage
       if not box_is_movable(state, box):
-        #print("Unmovable box for:\n")
-        #state.print_state()
-        #print("\n")
-        return float('inf')
+        return float('inf')   # Box is unmovable. State cannot be solved
+
+      # Closest robot to box
       closest_robot = float('inf')
       closest_robot_dist = float('inf')
+      # Closest storage to box
       closest_storage = float('inf')
       closest_storage_dist = float('inf')
+
+      # Find closest robot
       for i in range(len(robot_positions)):
         robot = robot_positions[i]
         dist = abs(box[0] - robot[0]) + abs(box[1] - robot[1])
         if dist < closest_robot_dist:
           closest_robot_dist = dist
           closest_robot = i
-      for j in range(len(remaining_storages)):
-        storage = remaining_storages[j]
+
+      # Find closest storage
+      for i in range(len(remaining_storages)):
+        storage = remaining_storages[i]
         dist = abs(box[0] - storage[0]) + abs(box[1] - storage[1])
         if dist < closest_storage_dist:
           closest_storage_dist = dist
-          closest_storage = j
+          closest_storage = i
+
+      # Cost to add to heuristic
       cost = closest_robot_dist + closest_storage_dist
-      robot = robot_positions[closest_robot]
-      for x in range(min(robot[0], box[0]), max(robot[0], box[0])):
-        if (x, robot[1]) in state.obstacles:
-          cost += 2
-      for y in range(min(robot[1], box[1]), max(robot[1], box[1])):
-        if (box[0], y) in state.obstacles:
-          cost += 2
+
+      # Increase cost for obstacles in shortest path
+      if len(state.obstacles) > 0:
+        robot = robot_positions[closest_robot]
+        for x in range(min(robot[0], box[0]), max(robot[0], box[0])):
+          if (x, robot[1]) in state.obstacles:
+            cost += 2
+        for y in range(min(robot[1], box[1]), max(robot[1], box[1])):
+          if (box[0], y) in state.obstacles:
+            cost += 2
+      
+      # After moving the box, the robot will be at the storage position, so move the robot there
       robot_positions[closest_robot] = remaining_storages[closest_storage]
+      # Remove storage spot from available storages
       remaining_storages.pop(closest_storage)
-      #h += closest_robot_dist + closest_storage_dist
+      # Add cost to heuristic
       h += cost
     return h
 
 def box_is_movable(state, box):
-  move_vertical = True
-  move_horizontal = True
+  move_vertical = True    # Box can move vertically
+  move_horizontal = True  # Box can move horizontally
 
   if box[0] == 0 or box[0] == state.width-1:
+    # Box is at a vertical edge, so can no longer move horizontally
     move_horizontal = False
     if box[0] == 0 and len([st for st in state.storage if st[0] == 0]) == 0:
-      return False
+      return False  # No storage spots along vertical edge of box. State is unsolvable
     if box[0] == state.width-1 and len([st for st in state.storage if st[0] == state.width-1]) == 0:
-      return False
+      return False  # No storage spots along vertical edge of box. State is unsolvable
 
   if box[1] == 0 or box[1] == state.height-1:
+    # Box is at a horizontal edge, so can no longer move vertically
     move_vertical = False
     if box[1] == 0 and len([st for st in state.storage if st[1] == 0]) == 0:
-      return False
+      return False  # No storage spots along horizontal edge of box. State is unsolvable
     if box[1] == state.height-1 and len([st for st in state.storage if st[1] == state.height-1]) == 0:
-      return False
+      return False  # No storage spots along horizontal edge of box. State is unsolvable
 
   if (box[0]-1, box[1]) in state.obstacles or (box[0]+1, box[1]) in state.obstacles:
+    # There is an obstacle to the left of right of the box, so the box cannot move horizontally
     move_horizontal = False
 
   if (box[0], box[1]-1) in state.obstacles or (box[0], box[1]+1) in state.obstacles:
+    # There is an obstacle above or below the box, so the box cannot move vertically
     move_vertical = False
 
   if not move_horizontal and not move_vertical:
-    return False
+    return False  # Box cannot move in either direction
 
   if not move_vertical:
-    #if (box[0]-1, box[1]) in state.boxes or (box[0]+1, box[1]) in state.boxes:
-    #  return False
-
+    # Box can supposedly move horizontally. Verify this by checking the boxes to its left or right
     if (box[0]-1, box[1]) in state.boxes:
+      # There is a box to the current box's left. Check if that box can move vertically (without considering any boxes
+      # that could be vertically adjacent to it)
       if box[1] == 0 or box[1] == state.height-1 or (box[0]-1, box[1]-1) in state.obstacles or (box[0]-1, box[1]+1) in state.obstacles:
-        return False
+        return False  # Current box cannot move because the box to its left cannot move away to free space
     if (box[0]+1, box[1]) in state.boxes:
+      # There is a box to the current box's right. Check if that box can move vertically (without considering any boxes
+      # that could be vertically adjacent to it)
       if box[1] == 0 or box[1] == state.height-1 or (box[0]+1, box[1]-1) in state.obstacles or (box[0]+1, box[1]+1) in state.obstacles:
-        return False
-
+        return False  # Current box cannot move because the box to its right cannot move away to free space
 
   if not move_horizontal:
-    #if (box[0], box[1]-1) in state.boxes or (box[0], box[1]+1) in state.boxes:
-    #  return False
-
+    # Box can supposedly move vertically. Verify this by checking the boxes above and below
     if (box[0], box[1]-1) in state.boxes:
+      # There is a box above the current box. Check if that box can move horizontally (without considering any boxes
+      # that could be horizontally adjacent to it)
       if box[0] == 0 or box[0] == state.width-1 or (box[0]-1, box[1]-1) in state.obstacles or (box[0]+1, box[1]-1) in state.obstacles:
-        return False
+        return False  # Current box cannot move because the box above it cannot move away to free space
     if (box[0], box[1]+1) in state.boxes:
+      # There is a box below the current box. Check if that box can move horizontally (without considering any boxes
+      # that could be horizontally adjacent to it)
       if box[0] == 0 or box[0] == state.width-1 or (box[0]-1, box[1]+1) in state.obstacles or (box[0]+1, box[1]+1) in state.obstacles:
-        return False
+        return False  # Current box cannot move because the box below it cannot move away to free space
 
-  return True
+  return True   # Box passes all reasonable checks and should be able to move (although not guaranteed)
 
 def heur_zero(state):
     '''Zero Heuristic can be used to make A* search perform uniform cost search'''
@@ -186,26 +205,28 @@ def anytime_weighted_astar(initial_state, heur_fn, weight=1., timebound = 10):
   '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
   '''OUTPUT: A goal state (if a goal is found), else False'''
   '''implementation of weighted astar algorithm'''
-  start = os.times()[0]
-  se = SearchEngine('astar', 'path')
-  wrapped_fval_function = (lambda sN : fval_function(sN, weight))
-  se.init_search(initial_state, sokoban_goal_state, heur_fn, wrapped_fval_function)
-  best_solution = se.search(timebound)
+  start = os.times()[0]   # Start Time
+  se = SearchEngine('custom', 'full')   # Create search engine
+  wrapped_fval_function = (lambda sN : fval_function(sN, weight))   # Wrapped fval function
+  se.init_search(initial_state, sokoban_goal_state, heur_fn, wrapped_fval_function)   # Init search engine
+  best_solution = se.search(timebound)    # Find initial solution with no pruning
   if not best_solution:
-    return False
-  new_timebound = timebound - (os.times()[0] - start)
-  while new_timebound > 0:
-      weight /= 2
-      if weight < 1:
-        weight = 1
-      new_solution = se.search(new_timebound, (best_solution.gval, best_solution.gval, best_solution.gval))
-      if not new_solution:
-          return best_solution
-      best_solution = new_solution
-      if weight == 1:
-        return best_solution
-      new_timebound = timebound - (os.times()[0] - start)
-  return best_solution
+    return False  # Could not find a solution
+  new_timebound = timebound - (os.times()[0] - start)   # Update timebound
+  while new_timebound > 0 and weight >= 1:    # No need to check for weights below 1
+    weight /= 2   # Update weight
+    if weight < 1:  # No need to check for weights below 1
+      weight = 1
+    # Obtain new solution by pruning with the best solution found so far
+    new_solution = se.search(new_timebound, (best_solution.gval, best_solution.gval, best_solution.gval))
+    if not new_solution:
+      # Could not find a better solution. Return best solution found so far
+      return best_solution
+    best_solution = new_solution  # Update best solution with new, better solution
+    if weight == 1:
+      return best_solution  # Weight of 1 outputs a provably optimal solution
+    new_timebound = timebound - (os.times()[0] - start) # Update timebound
+  return best_solution  # Return best solution found so far
 
 def anytime_gbfs(initial_state, heur_fn, timebound = 10):
 #IMPLEMENT
@@ -213,17 +234,19 @@ def anytime_gbfs(initial_state, heur_fn, timebound = 10):
   '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
   '''OUTPUT: A goal state (if a goal is found), else False'''
   '''implementation of weighted astar algorithm'''
-  start = os.times()[0]
-  se = SearchEngine('best_first', 'full')
-  se.init_search(initial_state, sokoban_goal_state, heur_fn)
-  best_solution = se.search(timebound)
+  start = os.times()[0]   # Start Time
+  se = SearchEngine('best_first', 'full')   # Update timebound
+  se.init_search(initial_state, sokoban_goal_state, heur_fn)   # Init search engine
+  best_solution = se.search(timebound)    # Find initial solution with no pruning
   if not best_solution:
-      return False
-  new_timebound = timebound - (os.times()[0] - start)
+      return False  # Could not find a solution
+  new_timebound = timebound - (os.times()[0] - start)    # Update timebound
   while new_timebound > 0:
-      new_solution = se.search(new_timebound, (best_solution.gval, best_solution.gval, best_solution.gval))
-      if not new_solution:
-          return best_solution
-      best_solution = new_solution
-      new_timebound = timebound - (os.times()[0] - start)
-  return best_solution
+    # Obtain new solution by pruning with the best solution found so far
+    new_solution = se.search(new_timebound, (best_solution.gval, best_solution.gval, best_solution.gval))
+    if not new_solution:
+      # Could not find a better solution. Return best solution found so far
+      return best_solution
+    best_solution = new_solution      # Update best solution with new, better solution
+    new_timebound = timebound - (os.times()[0] - start)    # Update timebound
+  return best_solution    # Return best solution found so far
